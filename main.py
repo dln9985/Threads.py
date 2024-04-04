83c867fcdde6a5da73928017dd7d4a503a852484
Diego López Naujokat <diego.l@uteca.edu.mx>
2024-04-04 08:44:03 -0600

    Commit tarea

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ for t in listaHilos:
     
 tf = time.time() - t0
 
-print(f"Tiempo total de ejecución: {tf}")
-"""
+print(f"Tiempo total de ejecución: {tf}")"""
+
 
 print("")
 print("Hilo Principal o 1 hilo")
@@ -65,7 +65,7 @@ for t in listaHilos:
 
 tf = time.time() - t0
 
-globalarrayNum.sort()
+
 print(f"Tiempo de ejecución: {tf}")
 print (globalarrayNum)
 
@@ -82,19 +82,27 @@ def EnumeradorTres( inicio, fin ):
         time.sleep(0.01)
     return 0
 
+Num_Hilos = 2
+
 t0 = time.time()
 listaHilos= []
-for i in range(4):
-    t = threading.Thread(target=EnumeradorTres, args=(1,100))
-    listaHilos.append(t)
+for i in range (Num_Hilos):
+    Val1= 1
+    Val2= 100
+    SumVeces= 100/Num_Hilos
+    t= threading.Thread(target=EnumeradorTres, args=(Val1,Val2)) #No puedo operar directamente en los args :(
+    listaHilos.append(t)                                         #¿Ciclo for pero en donde y con que variables?
     t.start()
+    
+#Se tarda exactamente lo mismo dado a que sigue siendo de 1 a 100
+#¿Que puedo hacer con SumVeces?: Entrar en un ciclo for.
 
 for t in listaHilos:
     t.join()
 
 tf = time.time() - t0
 
-globarrayNum.sort()
+
 print(f"Tiempo de ejecución: {tf}")
 print (globarrayNum)
 print("")
